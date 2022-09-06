eaa89ecf0f45d4fb5d3d869fbb441c99a55b141c
YangYu <yuyang123@stu.scu.edu.cn>
2022-09-06 17:36:19 +0800

    DE-PINN

--- Case2/NN_Config.py
+++ Case2/NN_Config.py
@@ -1,19 +1,19 @@
-"""Implementation of NEDP-PINN in Python 
-[NEDP-PINN](https://arxiv.org/abs/2208.13483 of our article in arXiv) (A comprehensive numerical study of PINN on
-solving neutron diffusion eigenvalue problem) is an PINN algorithm published by
-Yu Yang, Helin Gong*, Shiquan Zhang*, Qihong Yang, Zhang Chen, Qiaolin He, Qing Li in 2022.
-With this code, a comprehensive numerical study of PINN for solving neutron diffusion
-eigenvalue problem (NDEP) is brought out. A very small amount of prior points are used
-to improve the accuracy and efficiency of training, which can be obtained from physical
-experiments. We design an adaptive optimization procedure for training and also discuss
-the parameter dependence in the equations.Numerical results show that PINN with a few
-prior data can efficiently solve the problem with an appropriate optimization procedure
-for different parameters. The work confirms the possibility of PINN for practical
-engineering applications in nuclear reactor physics.
+"""Implementation of DEPINN in Python 
+[DEPINN](https://arxiv.org/abs/2208.13483 of our article in arXiv, GitHub: https://github.com/YangYuSCU/DEPINN) (A data-enabled physics-informed neural network 
+with comprehensive numerical study on solving neutron diffusion eigenvalue problems) We present a data-enabled physics-informed neural network (DEPINN) with 
+comprehensive numerical study for solving industrial scale neutron diffusion eigenvalue problems (NDEPs). In order to achieve an engineering acceptable accuracy 
+for complex engineering problems, a very small amount of prior data from physical experiments are suggested to be used, to improve the accuracy and efficiency of 
+training. We design an adaptive optimization procedure with Adam and LBFGS to accelerate the convergence in the training stage. We discuss the effect of different 
+physical parameters, sampling techniques, loss function allocation and the generalization performance of the proposed DEPINN model for solving complex problem. 
+The feasibility of proposed DEPINN model is tested on three typical benchmark problems, from simple geometry to complex geometry, and from mono-energetic equation 
+to two-group equations. Numerous numerical results show that DEPINN can efficiently solve NDEPs with an appropriate optimization procedure. The proposed DEPINN 
+can be generalized for other input parameter settings once its structure been trained. This work confirms the possibility of DEPINN for practical engineering 
+applications in nuclear reactor physics.
 
 All the computations are carried on NVIDIA A100(80G) and NVIDIA TITAN RTX. 
 
-Author: Yu Yang, Helin Gong*, Shiquan Zhang*, Qihong Yang, Zhang Chen, Qiaolin He, Qing Li.
+Author: Yu Yang(yuyang123@stu.scu.edu.cn), Helin Gong*(gonghelin@sjtu.edu.cn), Shiquan Zhang*(shiquanzhang@scu.edu.cn), Qihong Yang(yangqh@stu.scu.edu.cn), 
+Zhang Chen(chenzhang208@qq.com), Qiaolin He(qlhejenny@scu.edu.cn), Qing Li(liqing_xueshu@163.com).
 Yu Yang, Shiquan Zhang, Qihong Yang and Qiaolin He are with School of Mathematics, Sichuan University.
 Helin Gong is with ParisTech Elite Institute of Technology, Shanghai Jiao Tong University.
 Zhang Chen and Qing Li are with Science and Technology on Reactor System Design Technology Laboratory,
@@ -23,10 +23,12 @@ Code author: Yu Yang.
 Supported by: Qihong Yang.
 Reviewed by: Helin Gong, Qiaolin He and Shiquan Zhang.
 
+
+
 Copyright
 ---------
-NEDP-PINN is developed in School of Mathematics, Sichuan University, In collaboration with
-ParisTech Elite Institute of Technology, Shanghai Jiao Tong University. 
+DEPINN is developed in School of Mathematics, Sichuan University (Yu Yang, Shiquan Zhang, Qihong Yang, Qiaolin He), In collaboration with ParisTech Elite Institute 
+of Technology, Shanghai Jiao Tong University (Helin Gong). 
 More information about the technique can be found through corresponding authors.
  
 """
